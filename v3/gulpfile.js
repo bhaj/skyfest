6ec07feb8487c4de081c2e00d58e651a74679eed
bhaj <bhajaceratops@bhajaceratopss-MacBook-Pro.local>
2017-05-01 01:21:04 +0200

    dlkfjsla

---
+++ v3/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles',function(){
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('scss/*.scss',['styles']);
+  gulp.watch('./*.html').on('change',browserSync.reload);
+});
+
+gulp.task('default',['styles','serve']);
